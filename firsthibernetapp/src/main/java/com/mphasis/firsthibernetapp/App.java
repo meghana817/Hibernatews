e37f3d879014b9714fd43cb62ee66d81ca2c8412
meghana817 <meghanagoudb@gmail.com>
2026-03-25 12:32:55 +0530

    first commit

---
+++ firsthibernetapp/src/main/java/com/mphasis/firsthibernetapp/App.java
@@ -0,0 +1,10 @@
+package com.mphasis.firsthibernetapp;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
